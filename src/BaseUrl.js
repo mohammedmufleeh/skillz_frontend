8444f726a950bd773cf5ccbe58f34b5c3eacec2c
mohammedmufleeh <mmt7025@gmail.com>
2022-12-10 15:23:20 +0530

    added base url

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,4 +1,4 @@
-const BaseUrl = 'http://127.0.0.1:8000/api/';
-export const imageFolder = 'http://127.0.0.1:8000';
+const BaseUrl = 'http://15.206.93.94/api/';
+export const imageFolder = 'http://15.206.93.94';
 
 export default BaseUrl
