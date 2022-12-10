86705c653d30d009de41e693752de030541f4e20
mohammedmufleeh <mmt7025@gmail.com>
2022-12-10 15:52:18 +0530

    image error

--- src/components/student/Slogin.js
+++ src/components/student/Slogin.js
@@ -49,7 +49,7 @@ function Slogin() {
   <div class="container-fluid h-custom">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-md-9 col-lg-6 col-xl-5">
-        <img src="https://main.d1qye2yc6hthn1.amplifyapp.com/images.jpeg"
+        <img src="/images.jpeg"
           class="img-fluid" alt="Sample image"/>
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
